62c9d67c26b161a4553ed7ff91c636128db3225f
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-07-31 17:19:46 +0700

    CORS

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@ $(function () {
         "headers": {
             "Access-Control-Allow-Origin": "*",
             "Content-Type": "application/json",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
         }
     }
     const settingsIDAll = {
